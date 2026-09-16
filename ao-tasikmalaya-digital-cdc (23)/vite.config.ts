81216d275fc2141f863d01d4c6a0699c52faed6b
aldigarut08-web <aldi.garut08@gmail.com>
2026-09-17 00:44:31 +0700

    Update vite.config.ts

--- ao-tasikmalaya-digital-cdc (23)/vite.config.ts
+++ ao-tasikmalaya-digital-cdc (23)/vite.config.ts
@@ -70,6 +70,7 @@ export default defineConfig(() => {
       react(),
       tailwindcss(),
       aistudioMediaPlugin(),
+      base: './',
     ],
     build: {
       target: ['es2017', 'chrome58', 'edge79', 'firefox57', 'safari11'],
